1e620376645b4c36e25c472c85061e32199ad83e
Zuse <jcpjjz@gmail.com>
2025-11-22 02:40:45 -0800

    removed re from requirements

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import re
 
 # --- CONFIGURATION ---
 SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTR8Pa4QQVSNwepSe9dYnro3ZaVEpYQmBdZUzumuLL-U2IR3nKVh-_GbZeJHT2x9aCqnp7P-0hPm5Zd/pub?gid=221070242&single=true&output=csv"
