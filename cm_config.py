e21c3c14510cafa2aeea32f4c7494e601ab777c4
Zuse <jcpjjz@gmail.com>
2025-11-25 19:08:20 -0800

    Updated cm_config with more instructions

--- cm_config.py
+++ cm_config.py
@@ -1,10 +1,12 @@
+# update the relevant CM form_url with link when form is live/none when inactive
+# update sheet_url with the exploded CSV link from Google Sheets when available use the previous event's link as placeholder if needed
 CM_LIST = {
     "Virgo Cup (2025)": {
         "id": "cm5",
         "icon": "🏆",
         "sheet_url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vTR8Pa4QQVSNwepSe9dYnro3ZaVEpYQmBdZUzumuLL-U2IR3nKVh-_GbZeJHT2x9aCqnp7P-0hPm5Zd/pub?gid=221070242&single=true&output=csv",
         "parquet_file": "data/r2d2.parquet",
-        "form_url": "https://forms.gle/eDPAs8u4RHPKbxEN8",
+        "form_url": "https://forms.gle/eDPAs8u4RHPKbxEN8", 
         "status_msg": "Forms have closed for this event.",
         "guide_images": [
             "https://raw.githubusercontent.com/moomoocowsteam/canva/refs/heads/main/virgo.png"
