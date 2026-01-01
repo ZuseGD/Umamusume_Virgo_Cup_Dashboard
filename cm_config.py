d3daeba25cf90fd022d1a573be9c95a92b3d83be
James Zhang <jcpjjz@gmail.com>
2026-01-01 15:07:14 -0800

    Reordered CMs for Scorpio

--- cm_config.py
+++ cm_config.py
@@ -3,6 +3,22 @@ from decouple import config
 # update the relevant CM form_url with link when form is live/none when inactive
 # update sheet_url with the exploded CSV link from Google Sheets when available use the previous event's link as placeholder if needed
 CM_LIST = {
+    "Scorpio Cup (2025)": {
+        "id": "Scorpio Cup (2025)",
+        "icon": "⚖️",
+        "aptitude_dist": "Medium",   # Options: Sprint, Mile, Medium, Long
+        "aptitude_surf": "Turf",   # Options: Turf, Dirt
+        "sheet_url": config('', default=''),
+        "is_multipart_parquet": False,
+        "parquet_file": "libra.parquet" ,
+        "finals_csv": None, 
+        "finals_parquet": None,
+        "form_url": 'https://forms.gle/BUUMNG1MYZesYUjy7',
+        "status_msg": "Forms have not started yet.",
+        "guide_images": [
+            "https://raw.githubusercontent.com/moomoocowsteam/canva/refs/heads/main/scorpio.png"
+        ]
+    },
     "Libra Cup (2025)": {
         "id": "Libra Cup (2025)",
         "icon": "⚖️",
@@ -21,7 +37,7 @@ CM_LIST = {
         # Backwards compatibility flags
         "finals_csv": "data/cm6_finals.csv",
         "finals_parquet": None,
-        "form_url": None,
+        "form_url": 'https://forms.gle/BUUMNG1MYZesYUjy7',
         "status_msg": "Forms have closed for this event.",
         "guide_images": [
             "https://raw.githubusercontent.com/moomoocowsteam/canva/refs/heads/main/libra.png"
@@ -41,27 +57,12 @@ CM_LIST = {
         "finals_csv": "data/1_virgo_finals.csv", 
         "finals_parquet": "data/1_virgo_finals.parquet",
 
-        "form_url": None, 
+        "form_url": 'https://forms.gle/BUUMNG1MYZesYUjy7', 
         "status_msg": "Forms have closed for this event.",
         "guide_images": [
             "https://raw.githubusercontent.com/moomoocowsteam/canva/refs/heads/main/virgo.png"
         ]
     },
-    "Scorpio Cup (2025)": {
-        "id": "Scorpio Cup (2025)",
-        "icon": "⚖️",
-        "aptitude_dist": "Medium",   # Options: Sprint, Mile, Medium, Long
-        "aptitude_surf": "Turf",   # Options: Turf, Dirt
-        "sheet_url": config('', default=''),
-        "is_multipart_parquet": False,
-        "parquet_file": "libra.parquet" ,
-        "finals_csv": None, 
-        "finals_parquet": None,
-        "form_url": None,
-        "status_msg": "Forms have not started yet.",
-        "guide_images": [
-            "https://raw.githubusercontent.com/moomoocowsteam/canva/refs/heads/main/scorpio.png"
-        ]
-    },
+    
     
 }
